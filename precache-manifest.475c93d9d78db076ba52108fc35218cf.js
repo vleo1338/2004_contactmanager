0f3727967fb02bc3645eac0d917322086f1bc1e6
vleo1338 <vleo83@yahoo.com>
2020-04-20 16:09:58 +0800

    Updates

---
+++ precache-manifest.475c93d9d78db076ba52108fc35218cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ba0fa1e0eb82e0c41f4f0a046895cfa",
+    "url": "/2004_contactmanager/index.html"
+  },
+  {
+    "revision": "c152aad86b4ce01d3165",
+    "url": "/2004_contactmanager/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "c152aad86b4ce01d3165",
+    "url": "/2004_contactmanager/static/js/2.7b4c5c51.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/2004_contactmanager/static/js/2.7b4c5c51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0cb88197b3b0d5774bc4",
+    "url": "/2004_contactmanager/static/js/main.7853b27d.chunk.js"
+  },
+  {
+    "revision": "4da273aec904282d372f",
+    "url": "/2004_contactmanager/static/js/runtime-main.0148cc2c.js"
+  }
+]);
